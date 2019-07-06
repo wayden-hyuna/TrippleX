fdec9c9dcc376cabf957a4653777b0e52a7b1cff
wayden-hyuna <wayden.jones@hyuna.bb>
2019-07-06 17:15:40 -0400

    test

--- tripplex-helper.cpp
+++ tripplex-helper.cpp
@@ -34,7 +34,6 @@ bool playGame(int Difficulty)
     return checkSuccess(CodeSum, CodeProd, GuessSum, GuessProd, Difficulty);
 }
 
-
 bool checkSuccess(int CodeSum, int CodeProd, int GuessSum, int GuessProd, int Difficulty)
 {
     if (CodeSum == GuessSum && CodeProd == GuessProd && Difficulty != 10)
